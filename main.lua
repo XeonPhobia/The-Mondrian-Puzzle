a2b5c0ffe6aa0472edf9ea5a19d66cd3f7ab7cc2
XeonPhobia <XeonPhobia@users.noreply.github.com>
2018-10-17 14:13:59 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -20,71 +20,60 @@ for i = 1, tabel.size do
 end
 end
 
-
---[[
-local function addRectangle(n, i, j)
--- n is the table
-local losning = {}
-for i = 1, #n do 
-  if n[i][2] == n.size then
-    table.insert(losning, 'n[' .. i .. '][2] = ' .. n[i][2])
-  else
-    for j = i + 1, #n do 
-  if n[i][2] + n[j][2] <= n.size then  table.insert(losning, 'n[' .. i .. '][2] + n[' .. j .. '][2] = ' .. n[i][2] + n[j][2]) 
-  elseif n[i][2] + n[j][3] <= n.size then table.insert(losning, 'n[' .. i .. '][2] + n[' .. j .. '][3]' ) end
-end
-end
-end
-for i = 1, #losning do print(losning[i]) end
-loadstring("print(n[2][3])")
-end
---]]
-
-local function addRectangle(n, i, j)
--- n is the table
-
-local losning = {}
-local list = nil
-for i = 1, #n do 
-  if n[i][2] == n.size then
-    table.insert(losning, 'n[' .. i .. '][2] = ' .. n[i][2])
-  else
---    if n[i][2] < n.size then table.insert(midlertidig, {'n[' .. i .. '][2] = ' .. n[i][2], i, 2, n[i][2]}) end
---    if n[i][3] < n.size then table.insert(midlertidig, {'n[' .. i .. '][3] = ' .. n[i][3], i, 3, n[i][3]}) end
-    if n[i][2] < n.size then list = {next = list, value = {'n[' .. i .. '][2] = ' .. n[i][2], i, 2, n[i][2]}} end
-    if n[i][3] < n.size then list = {next = list, value = {'n[' .. i .. '][3] = ' .. n[i][3], i, 3, n[i][3]}} end
+local function compute(position, tabel, sum)
+  for i = position + 1, #tabel do
+    print("\n", i)
+     for i_next = i, #tabel do
+       io.write(tabel[i_next][1], " ")
+       local data = data or {}
+       table.insert(data, tabel[i_next][1]) 
+       check(position, tabel, sum)
+     end
   end
-
-end
-for i = 1, #losning do print(losning[i]) end
---for i = 1, #midlertidig do print(midlertidig[i][1]) end
---local l = list while l do print(l.value[1]) l = l.next end
-traverse(list)
 end
 
-function traverse(list)
-local list_output = nil
-  local l = list while l and l.next do 
-    print(l.value[1] .. ' + ' .. l.next.value[1], l.value[4] + l.next.value[4]) 
-    if l.value[4] + l.next.value[4] == 10 then list_output = {next = list_output, value = l.value[1] .. ' + ' .. l.next.value[1]} end     
-    l = l.next end
-
-
-local l = list_output while l do print(l.value) l = l.next end 
-
+function check(position, tabel, sum)
+if sum >= 20 then return 
+elseif sum == 20 then table.insert(list, pos, val) end
 end
 
-local function FindAllRectangles()
--- n[area][length][width]
-n = {{20, 5, 4}, {20, 10, 2}, {18, 6, 3}, {16, 8, 2}, {14, 7, 2}, {12, 6, 2}, size = 10}
-addRectangle(n)
+function expand(tabel, output)
+--if tabel[#tabel] == nil then return end
+--local position_val = tabel[#tabel].position
+local sum_val = tabel[#tabel].sum
+local tabel_val = table.remove(tabel)
+print("position_val","tabel_val[1]","sum_val")
+print(tabel_val.position, tabel_val[1], sum_val)
+
+  for i = tabel_val.position, #tabel.list do
+   
+    
+    if i > tabel_val.position then 
+      print("test", tabel_val.position, i)
+      if sum_val + tabel.list[i] < tabel.cutt_off then 
+        print("Lessthan20", sum_val + tabel.list[i], tabel_val[1] ) 
+        table.insert(tabel, {tabel_val.position .. '+' .. i, sum = sum_val + tabel.list[i], position = tabel_val.position + 1}) 
+      elseif sum_val + tabel.list[i] == tabel.cutt_off then 
+        print("Exactly20", sum_val + tabel.list[i], tabel_val[1] )  
+        table.insert(output, {tabel_val[1] .. '+' .. i, sum = sum_val + tabel.list[i], position = tabel_val.position})
+      else
+        print("out of bounds",  sum_val + tabel.list[i]) 
+      end
+    end
+  end
 end
 
 local function main()
-FindAllRectangles()
- Square = {};
-Square.size = 10;
-initiate(Square)
-mapPrinter(Square)
+nn_new = {{2}, {4}, {6}, {8}, {10}, {12}, list = {2, 4, 6, 8, 10, 12}, cutt_off = 20}
+  for i = 1, #nn_new do nn_new[i].sum = nn_new[i][1] end
+  for i = 1, #nn_new do nn_new[i].position = i end
+--compute(1, nn, 0)
+nn_output = {}
+  while #nn_new > 0 do expand(nn_new, nn_output) print("test2", nn_new[#nn_new][1]) end
+--  for i, v in ipairs(nn_new) do expand(nn_new, nn_output) end
+--  for i = 1, 4 do expand(nn_new, nn_output) end
+    for i = 1, #nn_new do print(nn_new[i][1]) end
+    for i, v in ipairs(nn_output) do print(nn_output[i][1], nn_output[i].sum) end
+    for i, v in ipairs(nn_new) do print("nn_new", nn_new[i][1], nn_new[i].sum) end
 end
 main()
